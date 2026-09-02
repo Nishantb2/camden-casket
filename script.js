3843bfdde49070b342fc2488b1f8776ede6ba243
Nishantb2 <40928880+Nishantb2@users.noreply.github.com>
2026-09-02 12:08:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-// Mobile navigation
+// =========================
+// Mobile Navigation
+// =========================
 
 const menuToggle = document.getElementById("menuToggle");
 const navMenu = document.getElementById("navMenu");
 
 menuToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
+  const isActive = navMenu.classList.toggle("active");
+
+  menuToggle.setAttribute("aria-expanded", isActive);
 });
 
 
-// Close mobile menu after clicking a link
+// =========================
+// Close mobile menu
+// after clicking a link
+// =========================
 
 const navLinks = document.querySelectorAll("#navMenu a");
 
 navLinks.forEach((link) => {
   link.addEventListener("click", () => {
     navMenu.classList.remove("active");
+    menuToggle.setAttribute("aria-expanded", "false");
   });
 });
 
 
-// Automatically update copyright year
+// =========================
+// Automatically update
+// copyright year
+// =========================
 
 document.getElementById("year").textContent =
   new Date().getFullYear();
